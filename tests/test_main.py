21d57e79f74829089aaf4d8c517c91828a0c9165
Suvorov.Alexandr <sasha.suvorov03@gmail.com>
2025-12-28 19:38:54 +1200

    Revert "Reapply "все без практики""
    This reverts commit 893400d46ffc88eff8681ac49f4d3e8715db657d.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,39 +1,15 @@
-from src.easy_test import add_numbers, reverse_string, list_avg, needing_word
+from src.easy_test import add_numbers, is_even, find_max, say_this, reverse_list
 from tests.conftest import my_list
-import pytest
-
-@pytest.mark.parametrize('num1, num2, summa', [
-    (2, 3, 5),
-    (5, 5, 10),
-    (20, 55, 75)
-])
-def test_easy_test(num1, num2, summa):
-    assert add_numbers(num1, num2) == summa
-
-@pytest.mark.parametrize("string, expected_result", [
-    ("sasha", "ahsas"),
-    ("suvorov", "vorovus"),
-    ("cool", "looc"),
-    ("", ""),
-])
-def test_reverse_string(string, expected_result):
-    assert reverse_string(string) == expected_result
 
 
-def test_list_avg(my_list):
-    assert list_avg(my_list) == 3
-    assert list_avg([]) == 0
+def test_easy_test(my_list):
+    # assert add_numbers(numbers, numbers) == 4
 
+    assert is_even(4)
+    assert is_even(3) == False
 
-@pytest.mark.parametrize('my_lst, my_type, result', [
-    ([1, '23', [1, 2], 3, {}, (3, 28)], int, 2),
-    ([1, '23', [1, 2], 3, {}, (3, 28)], str, 1),
-    ([], list, 0),
-    (['1, 23, [1, 2], 3, {}, (3, 28)'], dict, 0),
-    ([1, '23', [1, 2], 3, {}, (3, 28), '12', '23HHH', 34], int, 3),
-    (None, int, 0)
-])
-def test_needing_word(my_lst, my_type, result):
-    assert needing_word(my_lst, my_type) == result
+    # assert say_this(numbers) == 'hi'
 
-    # assert needing_word()
+    assert find_max(my_list) == 5
+
+    assert reverse_list(my_list) == [5, 4, 3, 2, 1]
